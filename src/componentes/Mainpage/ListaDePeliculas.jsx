48bf50dc141c8a53d82c5bb2ce4d36b31b73135d
Roman Mettler <Roman.Mettler@gmail.com>
2022-07-22 18:22:35 -0300

    Limpia ccs de lista de peliculas

--- src/componentes/Mainpage/ListaDePeliculas.jsx
+++ src/componentes/Mainpage/ListaDePeliculas.jsx
@@ -1,79 +1,136 @@
-import React from 'react';
-import { Card, Carousel, Spinner } from 'react-bootstrap';
-import { useFetchReducer } from '../../customHooks/useFetch';
-import { obtenerPeliculasTrending } from '../../service/API/obtenerDatosAPI';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faBookmark } from '@fortawesome/free-solid-svg-icons';
-import { arrayReduce } from '../../utilidades/utilidades';
-import { Link } from 'react-router-dom';
-import styled from '@emotion/styled';
+import React from "react";
+import { Card, Carousel, Spinner } from "react-bootstrap";
+import { useFetchReducer } from "../../customHooks/useFetch";
+import { obtenerPeliculasTrending } from "../../service/API/obtenerDatosAPI";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { arrayReduce } from "../../utilidades/utilidades";
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
 
 const ContenedorItems = styled.div`
-    display:flex;
-    margin-left: 30px;
-`
+  display: flex;
+  margin-left: 30px;
+  height: 400px;
+`;
 
-const Cards = ({ data }) => {
+const ContenedorThumbnail = styled(Card)`
+  width: 200px;
+  background-color: #1a1a1a;
+  margin-right: 20px;
+`;
 
+const ImagenThumbnail = styled(Card.Img)`
+  height: 250px;
+  width: auto;
+`;
+
+const IconoGuardarPelicula = styled(FontAwesomeIcon)`
+  color: black;
+  font-size: 35px;
+  width: auto;
+  position: absolute;
+  opacity: 0.9;
+`;
+
+const CuerpoDePelicula = styled(Card.Body)`
+  padding: 10px;
+  height: 165px;
+`;
+const PuntuacionPelicula = styled(Card.Text)`
+  color: #766a60;
+`;
+
+const TituloDePelicula = styled(Card.Text)`
+  color: white;
+  font-size: 18px;
+`;
+
+const ThumbnailPelicula = ({ data }) => {
+  return (
+    <Link to={`/movie/${data.id}`}>
+      <ContenedorThumbnail>
+        <ImagenThumbnail
+          variant="top"
+          src={`https://image.tmdb.org/t/p/w200${data.poster_path}`}
+        />
+        <IconoGuardarPelicula icon={faBookmark} />
+        <CuerpoDePelicula>
+          <PuntuacionPelicula>
+            <FontAwesomeIcon icon={faStar} style={{ color: "orange" }} />
+            {data.vote_average}
+          </PuntuacionPelicula>
+          <TituloDePelicula>{data.title}</TituloDePelicula>
+        </CuerpoDePelicula>
+      </ContenedorThumbnail>
+    </Link>
+  );
+};
+
+const ListaDePeliculas = ({
+  callbackAPI = obtenerPeliculasTrending,
+  tituloLista = "",
+  subTituloLista = "",
+}) => {
+  const { data, loading, error } = useFetchReducer(callbackAPI);
+  if (loading)
     return (
-        <Link to={`/movie/${data.id}`}>
-            <Card style={{ width: '200px', backgroundColor: "#1A1A1A", marginRight: "20px" }}>
-                <Card.Img style={{ height: "250px", width: "auto" }} variant="top" src={`https://image.tmdb.org/t/p/w200${data.poster_path}`} />
-                <FontAwesomeIcon icon={faBookmark} className="position-absolute" style={{ height: "40px", width: "auto", opacity: "0.9" }} />
-                <Card.Body style={{ height: "165px" }}>
-                    <Card.Text style={{ color: "#766A60" }}>
-                        <FontAwesomeIcon icon={faStar} style={{ color: "orange" }} />{data.vote_average}
-                    </Card.Text>
-                    <Card.Title style={{ color: "white" }}>{data.title}</Card.Title>
-                </Card.Body>
-            </Card>
-        </Link>
-    )
-}
+      <div
+        style={{
+          height: "380px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  if (error) return <div>error</div>;
+  if (data)
+    return (
+      <>
+        <div style={{ paddingTop: "10px", paddingBotom: "10px" }}>
+          <h3 style={{ color: "#FFC107" }}>
+            <strong>{tituloLista}</strong>
+          </h3>
+          {subTituloLista && (
+            <h5 style={{ color: "gray" }}>
+              <strong>{subTituloLista}</strong>
+            </h5>
+          )}
 
-const ListaDePeliculas = ({ callbackAPI = obtenerPeliculasTrending, tituloLista = "", subTituloLista = "" }) => {
-    const { data, loading, error } = useFetchReducer(callbackAPI)
-    if (loading)
-        return (
-            <div style={{ height: '380px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Spinner animation="border" role="status">
-                    <span className="sr-only">Loading...</span>
-                </Spinner>
-            </div>
-        )
-    if (error)
-        return (
-            <div>error</div>
-        )
-    if (data)
-        return (
-            <>
-                <div style={{ paddingTop: "10px", paddingBotom: "10px" }}>
-                    <h3 style={{ color: "#FFC107" }}><strong>{tituloLista}</strong></h3>
-                    {subTituloLista && <h5 style={{ color: "gray" }}><strong>{subTituloLista}</strong></h5>}
+          {data && (
+            <Carousel
+              interval={null}
+              indicators={false}
+              style={{
+                width: "100%",
+                height: "auto",
+                marginTop: "20px",
+                marginBottom: "20px",
+              }}
+            >
+              {arrayReduce(data, 5).map((x, i) => (
+                <Carousel.Item key={i}>
+                  <ContenedorItems>
+                    {x &&
+                      x.map((cardData, j) => (
+                        <ThumbnailPelicula
+                          data={cardData}
+                          key={j}
+                        ></ThumbnailPelicula>
+                      ))}
+                  </ContenedorItems>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          )}
+        </div>
+      </>
+    );
+};
 
-                    {data &&
-
-                        <Carousel interval={null} indicators={false} style={{ width: "100%", height: "auto", marginTop: "20px", marginBottom: "20px" }}>
-                            {arrayReduce(data, 5).map((x, i) =>
-                                <Carousel.Item key={i} >
-                                    <ContenedorItems>
-                                        {x && x.map((cardData, j) =>
-                                            <Cards data={cardData} key={j}></Cards>)}
-                                    </ContenedorItems>
-                                </Carousel.Item>
-                            )}
-
-                        </Carousel>
-                    }
-
-
-                </div>
-            </>
-        )
-
-
-
-}
-
-export default ListaDePeliculas
+export default ListaDePeliculas;
