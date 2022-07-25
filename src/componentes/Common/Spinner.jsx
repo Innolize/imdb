d34c780d86c648667241a3cde010b6dbb5153226
Roman Mettler <Roman.Mettler@gmail.com>
2022-07-25 11:41:11 -0300

    Centra trailer y cambia color de fondo en descripcion de pelicula

--- src/componentes/Common/Spinner.jsx
+++ src/componentes/Common/Spinner.jsx
@@ -3,14 +3,14 @@ import styled from "@emotion/styled";
 import { Spinner } from "react-bootstrap";
 
 const ContenedorSpinner = styled.div`
-  height: 380px;
   display: flex;
   justify-content: center;
   align-items: center;
+  height: ${(props) => (props.height ? props.height + "px" : "380px")};
 `;
 
-export const SpinnerPersonalizado = () => (
-  <ContenedorSpinner>
+export const SpinnerPersonalizado = ({ height }) => (
+  <ContenedorSpinner height={height}>
     <Spinner animation="border" role="status"></Spinner>
     <span className="sr-only">Loading...</span>
   </ContenedorSpinner>
--- src/componentes/DetallesPelicula/DetallesPelicula.jsx
+++ src/componentes/DetallesPelicula/DetallesPelicula.jsx
@@ -1,72 +1,74 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { useParams } from 'react-router-dom';
-import useFetchReducerWithCache from '../../customHooks/useFetchWithCache';
-import { buscarPeliculaPorID } from '../../service/API/obtenerDatosAPI';
-import Trailer from './Trailer';
-import InfoTitulo from './InfoTitulo';
-import DescripcionPelicula from './DescripcionPelicula';
+import React from "react";
+import styled from "@emotion/styled";
+import { useParams } from "react-router-dom";
+import useFetchReducerWithCache from "../../customHooks/useFetchWithCache";
+import { buscarPeliculaPorID } from "../../service/API/obtenerDatosAPI";
+import Trailer from "./Trailer";
+import InfoTitulo from "./InfoTitulo";
+import DescripcionPelicula from "./DescripcionPelicula";
 
-const MainInfo = styled.div`
-    
-`
+const MainInfo = styled.div``;
 const ContenedorPortada = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+`;
 const ImagenPortada = styled.img`
-    height: 480px;
-    padding-right: 5px;
-`
+  height: 400px;
+  padding-right: 5px;
+`;
 
 const LayoutPelicula = styled.div`
   background-color: #333333;
-  width:70%;
+  width: 70%;
   margin-left: auto;
   margin-right: auto;
   @media (min-width: 1600px) {
     width: 55%;
   }
-`
+`;
 
 const Wrapper = styled.div`
-    background-color:#D9D8D3;
-`
+  background-color: #d9d8d3;
+`;
 
-
+const ContenedorSinopsis = styled.div`
+  font-size: 18px;
+  color: white;
+  background-color: #333333;
+  padding: 15px 15px 0px 15px;
+`;
 
 export const Pelicula = () => {
-    const { idPelicula } = useParams()
-    const { data, loading, error } = useFetchReducerWithCache(buscarPeliculaPorID, idPelicula)
+  const { idPelicula } = useParams();
+  const { data, loading, error } = useFetchReducerWithCache(
+    buscarPeliculaPorID,
+    idPelicula
+  );
 
-    if (loading)
-        return <div>cargando</div>
+  if (loading) return <div>cargando</div>;
 
+  if (error) return <div>error</div>;
 
-    if (error)
-        return (
-            <div>error</div>
-        )
+  if (data)
+    return (
+      <Wrapper>
+        <LayoutPelicula>
+          <MainInfo>
+            <InfoTitulo data={data} />
+          </MainInfo>
+          <ContenedorPortada>
+            <ImagenPortada
+              src={`https://image.tmdb.org/t/p/original${data.poster_path}`}
+            />
+            <Trailer id={data.id} />
+          </ContenedorPortada>
+          <ContenedorSinopsis>{data.overview}</ContenedorSinopsis>
+          <DescripcionPelicula id={data.id} />
+        </LayoutPelicula>
+      </Wrapper>
+    );
+};
 
-    if (data)
-        return (
-            <Wrapper>
-                <LayoutPelicula>
-                    <MainInfo>
-                        <InfoTitulo data={data} />
-                    </MainInfo>
-                    <ContenedorPortada>
-                        <ImagenPortada src={`https://image.tmdb.org/t/p/original${data.poster_path}`} />
-                        <Trailer id={data.id} />
-                    </ContenedorPortada>
-                    <div style={{ fontSize: `18px`, color: `#333`, backgroundColor: `white`, padding: `15px 15px 0px 15px` }}>
-                        {data.overview}
-                    </div>
-                    <DescripcionPelicula id={data.id} />
-                </LayoutPelicula>
-            </Wrapper>
-        )
-}
-
-export default Pelicula
+export default Pelicula;
